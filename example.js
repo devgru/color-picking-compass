4d7e4ca274be59bec50bfa17e3e28991e735fe22
Dima Devgru <git@devg.ru>
2023-11-09 22:18:07 +0200

    Use different sets of primary colors

--- example.js
+++ example.js
@@ -14,31 +14,40 @@ function formatP3(color) {
 
 const toCents = roundToPrecision(0.01, Number);
 
-const primaryColors = [
-  rgb('rgb(0, 0, 0)'),
-  rgb('rgb(255, 0, 0)'),
-  rgb('rgb(255, 255, 0)'),
-  rgb('rgb(0, 255, 0)'),
-  rgb('rgb(0, 255, 255)'),
-  rgb('rgb(0, 0, 255)'),
-  rgb('rgb(255, 0, 255)'),
-  rgb('rgb(255, 255, 255)'),
-];
-
 const gamuts = [
   {
     name: 'sRGB',
     fitsGamut: displayable,
+    primaryColors: [
+      rgb('rgb(0, 0, 0)'),
+      rgb('rgb(255, 0, 0)'),
+      rgb('rgb(255, 255, 0)'),
+      rgb('rgb(0, 255, 0)'),
+      rgb('rgb(0, 255, 255)'),
+      rgb('rgb(0, 0, 255)'),
+      rgb('rgb(255, 0, 255)'),
+      rgb('rgb(255, 255, 255)'),
+    ],
   },
   {
     name: 'P3',
     fitsGamut: inP3,
+    primaryColors: [
+      p3('color(display-p3 0 0 0)'),
+      p3('color(display-p3 1 0 0)'),
+      p3('color(display-p3 1 1 0)'),
+      p3('color(display-p3 0 1 0)'),
+      p3('color(display-p3 0 1 1)'),
+      p3('color(display-p3 0 0 1)'),
+      p3('color(display-p3 1 0 1)'),
+      p3('color(display-p3 1 1 1)'),
+    ],
   },
 ];
 
 console.log('Looking for opposite color for each primary color:');
 
-gamuts.forEach(({ name, fitsGamut }) => {
+gamuts.forEach(({ name, primaryColors, fitsGamut }) => {
   console.log();
   console.log('Gamut', name);
   primaryColors.forEach(color => {
