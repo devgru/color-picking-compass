675fe165a014af39ad58d9ef0f849e6f0c2489d6
Dima Devgru <git@devg.ru>
2023-11-19 23:11:06 +0200

    Move scales-related utilities to lib/scales

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
-export { colorScale } from './lib/colorScale.js';
-export { findClosestPointOnScale } from './lib/findClosestPointOnScale.js';
-export { findOpposite } from './lib/findOpposite.js';
 export { formatHexOrCss } from './lib/formatHexOrCss.js';
 export { okLabDelta } from './lib/okLabDelta.js';
-export { stretchToGamut } from './lib/stretchToGamut.js';
+
+export { colorScale } from './lib/scales/colorScale.js';
+export { findClosestPointOnScale } from './lib/scales/findClosestPointOnScale.js';
+export { findOpposite } from './lib/scales/findOpposite.js';
+export { stretchToGamut } from './lib/scales/stretchToGamut.js';
