708132f09c3d40d2f2065cdba26fd38f07eeab8c
Dima Devgru <git@devg.ru>
2024-07-14 19:24:19 +0300

    Minor stuff

--- lib/polar/toLch.ts
+++ lib/polar/toLch.ts
@@ -4,9 +4,9 @@ import { Itp } from '../types';
 const normalizeHue = (hue: number) => ((hue = hue % 360) < 0 ? hue + 360 : hue);
 
 export const toLch = ({ i, t, p }: Itp): Lch => {
-  const halfT = t / 2;
+  const halfT = -t / 2;
   const c = Math.sqrt(halfT ** 2 + p ** 2);
-  const h = normalizeHue((Math.atan2(-halfT, p) * 180) / Math.PI);
+  const h = normalizeHue((Math.atan2(halfT, p) * 180) / Math.PI);
 
   return {
     mode: 'lch',
