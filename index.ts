c1feb8849a026436aa5452436095ad2a6884318c
Dima Devgru <git@devg.ru>
2024-08-07 23:18:59 +0300

    Cleanup

--- index.ts
+++ index.ts
@@ -1,17 +1,4 @@
 export * from './src/constants/origins';
-export * from './src/constants/rgbCubeVertices';
-
-export * from './src/grids/cubic/p3Grid';
-export * from './src/grids/cubic/p3Hull';
-export * from './src/grids/cubic/sRgbGrid';
-export * from './src/grids/cubic/sRgbHull';
-export * from './src/grids/cylindrical/hsDisc';
-export * from './src/grids/cylindrical/hslGrid';
-export * from './src/grids/cylindrical/hsvGrid';
-export * from './src/grids/cylindrical/okHslGrid';
-export * from './src/grids/cylindrical/okHsvGrid';
-export * from './src/grids/linear/p3MonochromeLine';
-export * from './src/grids/linear/sRgbMonochromeLine';
 
 export * from './src/scales/cartesianColorScale';
 export * from './src/scales/colorScale';
