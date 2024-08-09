12a6500292525a8e4f570352c90de00c0bbdd886
Dima Devgru <git@devg.ru>
2024-08-09 11:06:55 +0300

    Reimplement `findOppositeColor`

--- index.ts
+++ index.ts
@@ -2,5 +2,6 @@ export * from './src/constants/origins';
 
 export * from './src/scales/cartesianColorScale';
 export * from './src/scales/colorScale';
-export * from './src/scales/findOppositeColor';
 export * from './src/scales/itpCylindricalColorScale';
+
+export * from './src/utils/findOppositeColor';
---
+++ src/utils/findOppositeColor.ts
@@ -0,0 +1,34 @@
+import { cartesianColorScale, InGamutFn } from '../scales/cartesianColorScale';
+import { ColorInMode, Mode } from '../types/colors';
+import { colorScale } from '../scales/colorScale';
+
+export const findOppositeColor = <M extends Mode>(
+  pickedColor: ColorInMode<M>,
+  origin: ColorInMode<M>,
+  inGamut: InGamutFn<M>,
+): ColorInMode<M> => {
+  // `cartesianColorScale` is used to represent an interval between two colors
+  const scale = cartesianColorScale([pickedColor, origin]);
+
+  // we `stretch` the scale to reach gamut edges
+  const edgeToOppositeEdgeScale = scale.stretch(inGamut);
+
+  // with the stretched scale we can compute the position of the picked color in relation to the edge and the origin
+  // first, we get the color at the edge of the gamut
+  const edge = edgeToOppositeEdgeScale(0);
+
+  // then we create a new scale between the edge and the origin
+  const edgeToOrigin = cartesianColorScale([edge, origin]);
+
+  // we call `invert` on the scale to find the position of the picked color in relation to the edge and the origin
+  const closestPointOnScale = edgeToOrigin.invert(pickedColor);
+
+  // to compute the opposite color, we need to find the color with the similar offset from the edge
+  const oppositeEdge = edgeToOppositeEdgeScale(1);
+
+  // we create a new scale between the opposite edge and the origin
+  const oppositeEdgeToOrigin = colorScale([oppositeEdge, origin]);
+
+  // we return the color, offset from the opposite edge
+  return oppositeEdgeToOrigin(closestPointOnScale);
+};
